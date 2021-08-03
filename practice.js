43164d7404b08c38e1548ff3e42e269383014122
pjwhite08 <pamelajeanwhite@gmail.com>
2021-08-03 18:28:20 -0500

    solved

---
+++ practice.js
@@ -0,0 +1,19 @@
+let simpleArray = [1, 2, 3, 4, 5, 6,]
+
+let findIndex = simpleArray.indexOf(2+2)
+console.log(findIndex)
+
+// function idAtTemp(element, index, array){
+
+//   if (element["min_temp"] == 15.915) {
+//     console.log(element["min_temp"])
+//   //   console.log(element["id"])
+//   // return element["id"]
+//   }
+  
+// }
+
+//   const idealTemp = weather.forEach(idAtTemp)
+//   console.log();
+
+  //forEach(function callbackFn(element, index, array){ ... })
